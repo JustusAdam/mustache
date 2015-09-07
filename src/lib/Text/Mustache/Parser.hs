21a35239ec566be428e3f942b44087487eaa31de
Justus Adam <dev@justus.science>
2015-09-07 21:07:58 +0200

    Implemented delimiter change.

--- src/lib/Text/Mustache/Parser.hs
+++ src/lib/Text/Mustache/Parser.hs
@@ -20,6 +20,7 @@ import           Text.Parsec as P hiding (try)
 import           Text.ParserCombinators.Parsec
 import           Text.ParserCombinators.Parsec.Token
 import           Text.Printf
+import Data.Char
 
 
 data MustacheConf = MustacheConf { delimiters :: (String, String) }
@@ -32,6 +33,8 @@ invertedSectionBegin = "^"
 unescape2 = ("{", "}")
 unescape1 = "&"
 delimiterChange = "="
+allowedDelimiterCharacter =
+  satisfy $ not . or . sequenceA [ isSpace, isAlphaNum ]
 
 
 mustacheAllowedCharacters =
@@ -54,8 +57,8 @@ mustacheParser =
 parseTag :: String -> String -> MustacheParser String
 parseTag start end = do
   string start
-  skipMany space
-  manyTill mustacheAllowedCharacters (try (skipMany space >> string end))
+  spaces
+  mustacheAllowedCharacters `manyTill` try (skipMany space >> string end)
 
 
 
@@ -71,7 +74,7 @@ mustacheParseNode name = do
         , try isBegin >>= continue
         , try isInverted >>= continue
         , try isUnescaped >>= continue
-      -- <|> (isDelimiterChange >>= continue)
+        , try isDelimiterChange
         , try isPartial >>= continue
         , try isVariable >>= continue
         , try eof >> maybe (return []) (parserFail . ("Unclosed section " <>)) name
@@ -99,8 +102,16 @@ mustacheParseNode name = do
         isInverted = isTag invertedSectionBegin mempty >>= \sectionName ->
           MustacheInvertedSection sectionName <$> mustacheParseNode (return sectionName)
         isUnescaped = MustacheVariable False <$> (try (uncurry isTag unescape2) <|> isTag unescape1 mempty)
-        -- isDelimiterChange = do
-        --   contents <- isTag delimiterChange delimiterChange
+
+        isDelimiterChange = do
+          string (start <> delimiterChange)
+          delim1 <- allowedDelimiterCharacter `manyTill` space
+          spaces
+          delim2 <- allowedDelimiterCharacter `manyTill` try (string $ delimiterChange <> end)
+          oldState <- getState
+          putState $ oldState { delimiters = (delim1, delim2) }
+          mustacheParseNode name
+
         isPartial = MustachePartial <$> isTag partialBegin mempty
 
         isBegin =
