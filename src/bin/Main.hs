92bd42dadd83e90ed39b7d3952d4992233893a84
Justus Adam <dev@justus.science>
2015-09-18 16:28:35 +0200

    Better main file.

--- src/bin/Main.hs
+++ src/bin/Main.hs
@@ -4,10 +4,12 @@
 module Main (main) where
 
 
-import           Data.Aeson
+import           Control.Applicative             ((<|>))
+import           Data.Aeson                      (Value, eitherDecode)
 import qualified Data.ByteString                 as B
 import qualified Data.ByteString.Lazy            as BS
-import           Data.Foldable
+import           Data.Char                       (toLower)
+import           Data.Maybe                      (fromMaybe)
 import qualified Data.Text.IO                    as TIO
 import qualified Data.Yaml                       as Y
 import           System.Console.CmdArgs.Implicit
@@ -16,23 +18,30 @@ import           Text.Mustache
 
 
 data Arguments = Arguments
-  { template     :: FilePath
-  , templateDirs :: [FilePath]
-  , dataFiles    :: [FilePath]
+  { template     ∷ FilePath
+  , templateDirs ∷ [FilePath]
+  , dataFile     ∷ FilePath
+  , outputFile   ∷ Maybe FilePath
+  , dataFormat   ∷ Maybe String
   } deriving (Show, Data, Typeable)
 
 
 commandArgs ∷ Arguments
 commandArgs = Arguments
-  { template = def
+  { template  = def
       &= argPos 0
       &= typ "TEMPLATE"
-  , dataFiles = def
-      &= args
-      &= typ "DATA-FILES"
+  , dataFile  = def
+      &= argPos 1
+      &= typ "DATAFILE"
+  , outputFile = Nothing
+      &= help "Name of the resulting file"
+      &= typFile
   , templateDirs = ["."]
       &= help "The directories in which to search for the templates"
       &= typ "DIRECTORIES"
+  , dataFormat = Nothing
+      &= help "Format for input data"
   } &= summary "Simple mustache template subtitution"
 
 
@@ -44,27 +53,39 @@ readYAML ∷ FilePath → IO (Either String Value)
 readYAML = fmap Y.decodeEither . B.readFile
 
 
+readers ∷ [(String, FilePath → IO (Either String Value))]
+readers =
+  [ ("yaml", readYAML)
+  , ("yml" , readYAML)
+  , ("json", readJSON)
+  ]
+
+
+getReader ∷ String → Maybe (FilePath → IO (Either String Value))
+getReader = flip lookup readers . map toLower
+
+
 main ∷ IO ()
 main = do
-  a@(Arguments { template, templateDirs, dataFiles }) <- cmdArgs commandArgs
+  a@(Arguments { dataFormat, template, templateDirs, dataFile, outputFile })
+    ← cmdArgs commandArgs
 
   print a
   eitherTemplate ← compileTemplate templateDirs template
 
   case eitherTemplate of
     Left err → print err
-    Right compiledTemplate →
-      for_ dataFiles $ \file → do
+    Right compiledTemplate → do
 
-        let decoder =
-              case takeExtension file of
-                ".yml" → readYAML
-                ".yaml" → readYAML
-                _ → readJSON
-        decoded ← decoder file
+      let decode = fromMaybe readJSON $
+            (dataFormat >>= getReader)
+            <|> getReader (drop 1 $ takeExtension dataFile)
 
-        either
-          putStrLn
-          TIO.putStrLn
-          $ decoded >>=
-            substitute compiledTemplate . toMustache
+      let write = maybe TIO.putStrLn TIO.writeFile outputFile
+      decoded ← decode dataFile
+
+      either
+        putStrLn
+        write
+        $ decoded >>=
+          substitute compiledTemplate . toMustache
