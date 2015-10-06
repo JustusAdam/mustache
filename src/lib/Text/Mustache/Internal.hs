a310bd46d33a7bcdc6fa7e83ca814c688a9dcc56
Justus Adam <dev@justus.science>
2015-10-06 13:13:53 +0200

    Removed CPP.
    As suggested by @andrewthad in #4. Thank you.

--- src/lib/Text/Mustache/Internal.hs
+++ src/lib/Text/Mustache/Internal.hs
@@ -7,7 +7,6 @@ Maintainer  : dev@justus.science
 Stability   : experimental
 Portability : POSIX
 -}
-{-# LANGUAGE CPP           #-}
 {-# LANGUAGE UnicodeSyntax #-}
 module Text.Mustache.Internal (uncons) where
 
