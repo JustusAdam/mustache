44aa6f72f1771ba5243ad61c976bf6457ba554c8
Justus Adam <dev@justus.science>
2016-06-24 10:52:49 +0200

    Bumped version

--- src/Text/Mustache/Types.hs
+++ src/Text/Mustache/Types.hs
@@ -16,7 +16,7 @@ Portability : POSIX
 module Text.Mustache.Types
   (
   -- * Types for the Parser / Template
-    ASTree(..)
+    ASTree
   , STree
   , Node(..)
   , DataIdentifier(..)
@@ -49,7 +49,7 @@ import           Prelude.Unicode
 type STree = ASTree Text
 
 
-type ASTree α = [Node α] 
+type ASTree α = [Node α]
 
 
 -- | Basic values composing the STree
